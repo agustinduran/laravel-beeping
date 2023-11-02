1282991c812e944331256120f0126dcbfcda4909
Agustín Durán <agustineduran@gmail.com>
2023-11-02 06:10:20 -0300

    feat: title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Orders List Table</title>
     </head>
     <body >
         <livewire:orders-list />
