fb1c9ddec2c5abcec3edbde1e280be46617f1422
Agustín Durán <agustineduran@gmail.com>
2023-11-02 06:30:32 -0300

    feat: use and apply tailwindcss

--- resources/views/livewire/orders-list.blade.php
+++ resources/views/livewire/orders-list.blade.php
@@ -1,18 +1,20 @@
-<table>
-    <thead>
-        <tr>
-            <th>Order Ref</th>
-            <th>Customer Name</th>
-            <th>Total Qty</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($orders as $order)
+<div class="w-1/2 mx-auto mt-10 mb-10">
+    <table class="min-w-full divide-y divide-gray-200 border border-gray-300">
+        <thead class="bg-gray-800 text-white">
             <tr>
-                <td>{{ $order->order_ref }}</td>
-                <td>{{ $order->customer_name }}</td>
-                <td>{{ $order->total_qty }}</td>
+                <th class="border-t border-b w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Order Ref</th>
+                <th class="border-t border-b w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Customer Name</th>
+                <th class="border-t border-b text-center py-3 px-4 uppercase font-semibold text-sm">Total Qty</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody class="text-gray-700">
+            @foreach($orders as $order)
+                <tr>
+                    <td class="border-t w-1/3 text-left py-3 px-4">{{ $order->order_ref }}</td>
+                    <td class="border-t w-1/3 text-left py-3 px-4">{{ $order->customer_name }}</td>
+                    <td class="border-t text-center py-3 px-4">{{ $order->total_qty }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
